ae680452b5a196c263800df58dad1f21f0737239
JaumVituu <vitor40joao@gmail.com>
2026-08-12 00:00:56 -0300

    refactor(usuario): altera busca por email para DTO

--- src/main/java/dev/jhonjhonnys/estudos_spring/repository/UsuarioRepository.java
+++ src/main/java/dev/jhonjhonnys/estudos_spring/repository/UsuarioRepository.java
@@ -5,11 +5,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import dev.jhonjhonnys.estudos_spring.dto.usuario.UsuarioResponseDTO;
 import dev.jhonjhonnys.estudos_spring.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    Optional<Usuario> findByEmail (String email);
+    Optional<UsuarioResponseDTO> findByEmail (String email);
 
     boolean existsByEmail(String email);
 }
